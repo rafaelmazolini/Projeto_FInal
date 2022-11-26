2e9dbfb780ab07f727dbcd8e2bf8c1494c254ec8
rafaelmazolini <rafael.mazolinif@gmail.com>
2022-11-25 22:08:28 -0300

    oi

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -4,6 +4,13 @@
 
 @section ('content')  
 
-<h1> Algum titulo </h1>
+@foreach($cursos as $curso)
+
+  <h1>CURSOS</h1>
+    
+    <h3>{{$curso -> nome}}</h3>
+    <p>{{ $curso -> descricao_simplificada }}</p>
+  
+  @endforeach
 
 @endsection
